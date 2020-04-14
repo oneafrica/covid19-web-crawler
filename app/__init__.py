a945c3544225b7aefff8ff3dff9989e5e43623c9
Emmanuel Ndangurura <endangurura@tachyon.engineering>
2020-04-15 02:45:56 +0300

    add LICENSE

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+
+from app.routes.country import country_blueprint
+from app.routes.statistics import statistics_blueprint
+from app.routes.yesterday import yesterday_blueprint
+from app.routes.home import home_blueprint
+from app.utils import fill_country_object
+
+app = Flask(__name__)
+
+app.register_blueprint(country_blueprint)
+app.register_blueprint(yesterday_blueprint)
+app.register_blueprint(statistics_blueprint)
+app.register_blueprint(home_blueprint)
